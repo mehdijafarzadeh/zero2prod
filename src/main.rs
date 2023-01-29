92eb8700da7350c6137e635321f70f530820a01c
mehdijafarzadeh <jafarzade.me@gmail.com>
2023-01-29 17:25:09 +0100

    open in intellij

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-use actix_web::{web,App,HttpServer,Responder, HttpResponse};
- 
+use zero_to_prod::run;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-        HttpServer::new(|| {
-                App::new()
-                    .route("/health_check", web::get().to(health_check))
-                    
-            })
-            .bind("127.0.0.1:8000")?
-            .run()
-            .await
+            run().await
     
 } 
     
-    
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[cfg(test)]
-mod tets{
-    use super::*;
-    
-    async fn health_check_succeed(){
-        let response = health_check().await;
-        
-        assert!(response.status().is_success())
-    }
-}
