2f486345e3eee7c4416e6f1c283341acba3ee30a
mehdijafarzadeh <jafarzade.me@gmail.com>
2023-01-29 15:43:15 +0100

    add from mac

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use actix_web::{web,App,HttpRequest,HttpServer,Responder};
-async fn greet(req:HttpRequest)->impl Responder{
-    let name=req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!",&name)
+use actix_web::{web,App,HttpServer,Responder, HttpResponse};
+ 
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+        HttpServer::new(|| {
+                App::new()
+                    .route("/health_check", web::get().to(health_check))
+                    
+            })
+            .bind("127.0.0.1:8000")?
+            .run()
+            .await
+    
 }
     
-#[tokio::main]
-async fn main()->std::io::Result<()>{
-    HttpServer::new(||{
-        App::new()
-        .route("/",web::get().to(greet))
-        .route("/{name}",web::get().to(greet))
-        })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[cfg(test)]
+mod tets{
+    use super::*;
+    
+    async fn health_check_succeed(){
+        let response = health_check().await;
+        
+        assert!(response.status().is_success())
+    }
 }
