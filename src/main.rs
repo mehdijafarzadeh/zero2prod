19c2cd040ef577aa9c451b530ac4f1c0fff167a6
mehdijafarzadeh <jafarzade.me@gmail.com>
2023-04-03 23:27:57 +0200

    Add database, sqlx, migration and separate the files.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::net::TcpListener;
 use zero_to_prod::run;
+use zero_to_prod::configuration::get_configuration;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
